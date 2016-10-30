36d8b989a93dc1effabb10fabff660de692be88c
Ken Lathrop <gamingsciences@gmail.com>
2016-10-30 10:00:18 -0700

    Added Sqlalchemy-Marshmallow
    Using Marshmallow and Sqlalchemy for object serialization.

--- chinook_db/data_extract.py
+++ chinook_db/data_extract.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 """
+The data_extract program uses Sqlalchemy automap to reflect the target 
+database.  Marshmallow is then used to serialize the data.
+
+It is the first part in the ETL process for ElasticStar.
+
 Created on Sat Feb 06 15:48:10 2016
 
 @author: Lathropk
@@ -8,24 +13,128 @@ from django.conf import settings
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import MetaData
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+from utils.database_utils import _gen_relationship
+
 
 
 engine = settings.CHINOOK_ENGINE
 metadata = MetaData()
 metadata.reflect(engine, only=['Album', 
-                               'Artist'])
+                               'Artist',
+                               'Customer',
+                               'Employee',
+                               'Genre',
+                               'Invoice',
+                               'InvoiceLine',
+                               'MediaType',
+                               'Playlist',
+                               'PlaylistTrack',
+                               'Track'])
 
 Base = automap_base(metadata=metadata)
-Base.prepare()
+    
+Base.prepare(generate_relationship=_gen_relationship)
 
 Session = sessionmaker(bind=engine)
 
+# some of the reflected tables don't generate a Base class like PlaylistTrack
+# usually they are tables that don't have a primary key and are many-to-many 
+# tables. 
+
 Album = Base.classes.Album
 Artist = Base.classes.Artist
+Customer = Base.classes.Customer
+Employee = Base.classes.Employee
+Genre = Base.classes.Genre
+Invoice = Base.classes.Invoice
+InvoiceLine = Base.classes.InvoiceLine
+MediaType = Base.classes.MediaType
+Playlist = Base.classes.Playlist
+Track = Base.classes.Track
 
-def test_extract():
-    session = Session()
-    query = session.query(Album, Artist).join('ArtistId')
-    session.close()
+# Create the Marshmallow Schemas for each Base Class
+# also add nested object schema logic. Be mindful of the order of these classes
+# Nested schemas must come before the primary schema
+
+class ArtistSchema(ModelSchema):
+    class Meta:
+        model = Artist
+
+
+class AlbumSchema(ModelSchema):
+    artist = fields.Nested(ArtistSchema, exclude=('ArtistId',
+                                                  'album_collection'))
+    class Meta:
+        model = Album
+        
+
+class CustomerSchema(ModelSchema):
+    class Meta:
+        model = Customer
+
+        
+class EmployeeSchema(ModelSchema):
+    class Meta:
+        model = Employee
+
+
+class GenreSchema(ModelSchema):
+    class Meta:
+        model = Genre
+
+        
+class MediaTypeSchema(ModelSchema):
+    class Meta:
+        model = MediaType
+ 
+       
+class TrackSchema(ModelSchema):
+    album = fields.Nested(AlbumSchema, exclude=('track_collection',
+                                                'AlbumId'))
+    genre = fields.Nested(GenreSchema, exclude=('track_collection',
+                                                'GenreId'))
+    mediatype = fields.Nested(MediaTypeSchema, exclude=('track_collection',
+                                                        'MediaTypeId'))
+    class Meta:
+        model = Track
+
+
+class InvoiceLineSchema(ModelSchema):
+    track = fields.Nested(TrackSchema, exclude=('playlist_collection',
+                                                'UnitPrice',
+                                                'invoiceline_collection'))
+    class Meta:
+        model = InvoiceLine
+
+        
+class InvoiceSchema(ModelSchema):
+    invoiceline_collection = fields.Nested(InvoiceLineSchema, many=True, 
+                                           exclude=('invoice',))
+    class Meta:
+        model = Invoice
+
+
+class PlaylistSchema(ModelSchema):
+    class Meta:
+        model = Playlist
+
+# Create your schema objects
+
+album_schema = AlbumSchema()
+artist_schema = ArtistSchema()
+customer_schema = CustomerSchema()
+employee_schema = EmployeeSchema()
+genre_schema = GenreSchema()
+invoiceline_schema = InvoiceLineSchema()
+invoice_schema = InvoiceSchema()
+mediatype_schema = MediaTypeSchema()
+playlist_schema = PlaylistSchema()
+track_schema = TrackSchema()
+
+def serialize(schema, obj):
+    dump_data = schema.dump(obj).data
     
-    return query.all()
+    return dump_data
+
---
+++ utils/database_utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 09:46:38 2016
+
+@author: ken
+"""
+from sqlalchemy.ext.automap import generate_relationship
+from sqlalchemy.orm import interfaces
+
+
+def _gen_relationship(base, direction, return_fn,
+                                attrname, local_cls, referred_cls, **kw):
+    if direction is interfaces.ONETOMANY:
+        kw['cascade'] = 'all, delete-orphan'
+        kw['passive_deletes'] = True
+    # make use of the built-in function to actually return
+    # the result.
+    return generate_relationship(base, direction, return_fn,
+                                 attrname, local_cls, referred_cls, **kw)
